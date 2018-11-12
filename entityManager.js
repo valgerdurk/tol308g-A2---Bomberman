47b044a9acbf2237a164e9b9f6b96a1ff2237145
Nevatrue <nevatru@gmail.com>
2018-11-12 03:45:15 +0000

    Camera update
    We can now scale up and down the map and canvas and the camera should probably, most likely, hopefully still work as intended.
    
    for the most part it is just ctx.translate magic with what could be considered too many if and else if statements.

--- entityManager.js
+++ entityManager.js
@@ -79,8 +79,70 @@ var entityManager = {
 
     camera: function(ctx) {
         var pl = this._player[0];
+        var hw = g_canvas.width/2;
+        var hh = g_canvas.height/2;
+        var xEdge = (-pl.cx+hw);
+        var yEdge = (-pl.cy+hh);
+        // these prob wont change but now they scale properly
+        var Wmargin = (g_map.tileWidth * 3) - g_canvas.width;
+        var Hmargin = (g_map.tileHeight * 3) - g_canvas.height;
         ctx.save();
-        ctx.translate(-pl.cx+g_canvas.width/2,-pl.cy+g_canvas.height/2);
+        
+        //ctx.translate(-pl.cx+hw,-pl.cy+hh);
+        /*
+        console.log("xedge " + xEdge);
+        console.log("yedge " + yEdge);
+        console.log("margin " +margin);
+        */
+        //far left edge of screen
+        if(xEdge >= 0){
+            //top edge
+            if(yEdge >= 0){
+                //top left corner 
+                //lock both x and y axix
+                ctx.translate(0,0);
+            }
+            else if(yEdge <= Hmargin) {
+                // in this case the -448 needs be something else
+                //lock in both x and y axis
+                ctx.translate(0, Hmargin);
+            } else {
+                //continue moving up or down
+                //but lock in x axis
+                ctx.translate(0, yEdge);
+            }
+        } //check other x border
+        else if (xEdge <= Wmargin) {
+            //far most right edge
+            if(yEdge >= 0) {
+                //lock x and y axis
+                ctx.translate(Wmargin,0);
+            }
+            else if(yEdge <= Hmargin) {
+                //in this case the -448 needs be something else
+                //lock in both x and y axis
+                ctx.translate(Wmargin, Hmargin);
+                } 
+                else {
+                    ctx.translate(Wmargin, yEdge);
+                }
+            } //assuming all other conditions are met, we can freely move about
+            else {
+                if(yEdge >= 0){
+                    // lock y axix top
+                    ctx.translate(xEdge, 0);
+                } 
+                else if (yEdge <= Hmargin) {
+                    // lock y axix bot
+                    ctx.translate(xEdge, Hmargin);
+                } else {
+                    // all otther conditions met, move up n down
+                    ctx.translate(xEdge, yEdge);
+                }
+                
+            }
+
+
     }
 
 };
