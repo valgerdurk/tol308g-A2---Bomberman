29c2936f5cf6dd806546be7096ddb9fbac6f3bcf
unknown <ema22@hi.is>
2018-11-20 20:24:15 +0000

    fixed sound

--- GameMenu.js
+++ GameMenu.js
@@ -22,9 +22,13 @@ Game.prototype.cx = 320;
 Game.prototype.cy = 320;
 
 Game.prototype.KEY_RESTART = '\r\n'.charCodeAt(0);
+Game.prototype.KEY_WIN = 'L'.charCodeAt(0);
 
 Game.prototype.render = function (ctx) {
-    
+    if(eatKey(this.KEY_WIN)){
+        g_winGame = !g_winGame;
+    }
+
         if(g_isUpdatePaused){ 
             this.sprite = g_sprites[55]
             this.sprite.drawCentredAt(ctx,this.cx,this.cy);
@@ -34,7 +38,15 @@ Game.prototype.render = function (ctx) {
             this.sprite = g_sprites[57]
             this.sprite.drawCentredAt(ctx,this.cx,this.cy);
             if(eatKey(this.KEY_RESTART)){
-                document.location.reload(true);
+                g_sounds.playSelect2();
+
+                //Slight delay on the restart so the select sound can play.
+                var delayInMilliseconds = 300;
+                setTimeout(function() {
+                    document.location.reload(true);
+                }, 
+                delayInMilliseconds);
+               
         }
     }
 };
